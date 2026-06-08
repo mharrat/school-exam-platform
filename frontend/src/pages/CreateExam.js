979856b5848b32ef706e6387a511867da803d028
mharrat <tweneboah123226@gmail.com>
2026-06-08 17:05:53 +0000

    Add create exam page

---
+++ frontend/src/pages/CreateExam.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CreateExam() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-8">
+        <h1 className="text-3xl font-bold mb-8">Create New Exam</h1>
+        <p className="text-gray-600">Exam creation interface - to be implemented</p>
+      </div>
+    </div>
+  );
+}
+
+export default CreateExam;
